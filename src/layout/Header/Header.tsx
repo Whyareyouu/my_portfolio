91b2a1b7ab82e8828474b58f5dd1876fdaf6f27a
whyareyou <bionixxxd5@gmail.com>
2023-02-11 23:08:46 +0300

    added routes for the header component

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -7,6 +7,7 @@ import ProjectsIcon from '../../assets/icons/projects.svg';
 import ResumeIcon from '../../assets/icons/resume.svg';
 import ContactIcon from '../../assets/icons/contact.svg';
 import { HandySvg } from 'handy-svg';
+import { Link } from 'react-router-dom';
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
 	return (
 		<nav
@@ -14,25 +15,35 @@ export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
 			{...props}>
 			<img src={Logo} alt='Logo' className='w-16' />
 			<ul className='flex gap-6 text-lg'>
-				<li className='flex gap-2'>
-					<HandySvg src={HomeIcon} width='24' height='24' />
-					<span>Home</span>
+				<li>
+					<Link className='flex gap-2' to='/'>
+						<HandySvg src={HomeIcon} width='24' height='24' />
+						<span>Home</span>
+					</Link>
 				</li>
-				<li className='flex gap-2'>
-					<HandySvg src={ProfileIcon} width='24' height='24' />
-					<span>About</span>
+				<li>
+					<Link className='flex gap-2' to='/about'>
+						<HandySvg src={ProfileIcon} width='24' height='24' />
+						<span>About</span>
+					</Link>
 				</li>
-				<li className='flex gap-2'>
-					<HandySvg src={ProjectsIcon} width='24' height='24' />
-					<span>Projects</span>
+				<li>
+					<Link className='flex gap-2' to='/projects'>
+						<HandySvg src={ProjectsIcon} width='24' height='24' />
+						<span>Projects</span>
+					</Link>
 				</li>
-				<li className='flex gap-2'>
-					<HandySvg src={ContactIcon} width='24' height='24' />
-					<span>Contact</span>
+				<li>
+					<Link className='flex gap-2' to='/contact'>
+						<HandySvg src={ContactIcon} width='24' height='24' />
+						<span>Contact</span>
+					</Link>
 				</li>
-				<li className='flex gap-2'>
-					<HandySvg src={ResumeIcon} width='24' height='24' />
-					<span>Resume</span>
+				<li>
+					<Link className='flex gap-2' to='/resume'>
+						<HandySvg src={ResumeIcon} width='24' height='24' />
+						<span>Resume</span>
+					</Link>
 				</li>
 			</ul>
 		</nav>
