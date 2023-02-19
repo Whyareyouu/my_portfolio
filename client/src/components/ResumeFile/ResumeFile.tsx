7e6185537cb73b48b7a7622c3bc953e761207471
whyareyou <bionixxxd5@gmail.com>
2023-02-19 12:31:31 +0300

    add adaptive for resume page

--- client/src/components/ResumeFile/ResumeFile.tsx
+++ client/src/components/ResumeFile/ResumeFile.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack5';
 import { ResumeProps } from '../../pages/Resume/Resume.props';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 export const ResumeFile = ({ ...props }: ResumeProps): JSX.Element => {
 	const LINK =
 		'https://raw.githubusercontent.com/Whyareyouu/my_portfolio/master/client/src/assets/resumeEN.pdf';
+	const [width, setWidth] = useState<number>(1200);
+
+	useEffect(() => {
+		setWidth(window.innerWidth);
+	}, []);
 	return (
 		<div className='flex justify-center' {...props}>
 			<Document className='relative' file={LINK} externalLinkTarget='_blank'>
 				<Page
+					scale={width > 768 ? 1 : 0.5}
 					className='md:max-w-[320px]'
 					pageNumber={1}
 					renderTextLayer={false}
--- client/src/pages/Resume/Resume.tsx
+++ client/src/pages/Resume/Resume.tsx
@@ -6,19 +6,22 @@ import ResumeRU from '../../assets/resumeRU.pdf';
 export const Resume = ({ className, ...props }: ResumeProps): JSX.Element => {
 	return (
 		<div
-			className={cn(className, 'flex flex-col gap-4 text-lg max-h-full')}
+			className={cn(
+				className,
+				'flex flex-col gap-4 text-lg max-h-full md:text-base'
+			)}
 			{...props}>
 			<a
 				href={ResumeEN}
 				download
-				className='bg-purpledark max-w-[220px] rounded-md p-3 self-center font-bold'>
+				className='bg-purpledark max-w-[220px] rounded-md p-3 self-center font-bold md:p-2'>
 				<button>Download resume EN</button>
 			</a>
 			<ResumeFile />
 			<a
 				href={ResumeRU}
 				download
-				className='bg-purpledark max-w-[220px] rounded-md p-3 self-center font-bold'>
+				className='bg-purpledark max-w-[220px] rounded-md p-3 self-center font-bold md:p-2'>
 				<button>Download resume RU</button>
 			</a>
 		</div>
