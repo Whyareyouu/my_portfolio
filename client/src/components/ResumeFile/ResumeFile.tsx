34006392bc12be4ce23863664e640b5f79e89179
whyareyou <bionixxxd5@gmail.com>
2023-02-19 12:21:18 +0300

    fix bug with resume

--- client/src/components/ResumeFile/ResumeFile.tsx
+++ client/src/components/ResumeFile/ResumeFile.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
-import resume from './resumeEN.pdf';
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack5';
 import { ResumeProps } from '../../pages/Resume/Resume.props';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 export const ResumeFile = ({ ...props }: ResumeProps): JSX.Element => {
+	const LINK =
+		'https://raw.githubusercontent.com/Whyareyouu/my_portfolio/master/client/src/assets/resumeEN.pdf';
 	return (
 		<div className='flex justify-center' {...props}>
-			<Document className='relative' file={resume} externalLinkTarget='_blank'>
+			<Document className='relative' file={LINK} externalLinkTarget='_blank'>
 				<Page
 					className='md:max-w-[320px]'
 					pageNumber={1}
