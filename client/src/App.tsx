3bb4658d47e24fb00cdf9ed6ff524fe2b86da66a
whyareyou <bionixxxd5@gmail.com>
2023-02-20 15:45:25 +0300

    added loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+import { Triangle } from 'react-loader-spinner';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 import { About, Home, NotFoundPage, Projects, Resume } from './pages';
@@ -6,16 +8,27 @@ import { Contact } from './pages/Contact/Contact';
 function App() {
 	return (
 		<div className='container'>
-			<Layout>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/projects' element={<Projects />} />
-					<Route path='/contact' element={<Contact />} />
-					<Route path='/resume' element={<Resume />} />
-					<Route path='*' element={<NotFoundPage />} />
-				</Routes>
-			</Layout>
+			<Suspense
+				fallback={
+					<Triangle
+						height='160'
+						width='160'
+						color='#ff77e9'
+						ariaLabel='triangle-loading'
+						visible={true}
+					/>
+				}>
+				<Layout>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/projects' element={<Projects />} />
+						<Route path='/contact' element={<Contact />} />
+						<Route path='/resume' element={<Resume />} />
+						<Route path='*' element={<NotFoundPage />} />
+					</Routes>
+				</Layout>
+			</Suspense>
 		</div>
 	);
 }
