de6908b678a4cfbf4523222383285c0fcf6a92ef
whyareyou <bionixxxd5@gmail.com>
2023-02-23 21:02:16 +0300

    added notfoundpage

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,24 @@
 import { NotFoundPageProps } from './NotFoundPage.props';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 export const NotFoundPage = ({
 	className,
 	...props
 }: NotFoundPageProps): JSX.Element => {
 	return (
-		<div className={cn(className, '')} {...props}>
-			<h1>Page notfound</h1>
+		<div
+			className={cn(
+				className,
+				'flex flex-col w-full justify-center items-center text-3xl h-full font-medium'
+			)}
+			{...props}>
+			<h1>The page you are trying to get to does not exist.</h1>
+			<h2>
+				Go back to the{' '}
+				<Link to='/' className='text-bubble-gum font-bold'>
+					Home page
+				</Link>
+			</h2>
 		</div>
 	);
 };
